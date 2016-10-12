ab061c8d8168aa55f3511718e4145541f1d3781c
akhilsbehl <akhilsbehl@gmail.com>
2016-10-12 20:11:53 +0530

    Renames to make generic.

--- src/utils.lua
+++ src/utils.lua
@@ -27,8 +27,36 @@ local function eval_string(s)
    return loadstring('return ' .. s)()
 end
 
-local function load_data(data_dir)
+local function load_data(dataset, validation_ratio)
+   local loaders = {
+      mnist = load_mnist,
+      -- cifar10 = load_cifar10,
+   }
+   local train_, test = loaders[dataset]()
+   local train, validation = make_validation(train_, validation_ratio)
+   return {
+      train = train,
+      validation = validation,
+      test = test,
+   }
+
 end
+
+local function load_mnist()
+   local train = torch.load(data_dir .. '/train.t7', 'ascii')
+   train['records'] = train['data']
+   train.data = nil
+   local test = torch.load(data_dir .. '/test.t7', 'ascii')
+   test['records'] = test['data']
+   test.data = nil
+   return train, test
+end
+
+local function make_validation(train, validation_ratio)
+   local train_size = train['labels']:size(1)
+   local validation_size = math.ceil(validation_ratio * train_size)
+   local validation_indices =
+      torch.randperm(train_size)[{ {1, validation_size} }]
 
 -- 2. Exports
 
